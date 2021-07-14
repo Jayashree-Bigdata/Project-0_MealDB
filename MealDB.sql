67143ac0cfa95136ec11388b999fb7e1482e58f1
Jayashree150396 <86697664+Jayashree150396@users.noreply.github.com>
2021-07-14 13:44:23 +0530

    Update MealDB.sql

--- MealDB.sql
+++ MealDB.sql
@@ -1,3 +1,5 @@
+-- To Fetch the information from MealDB
+
 SELECT * FROM MealDB;
 
 -- To select a row from table
@@ -11,18 +13,20 @@ SELECT * FROM MealDB limit 5;
 -- Delete
 
 DELETE from MealDB WHERE meal_id = 1438;
+
 SELECT * FROM MealDB WHERE meal_id = 1438;
 
 
--- Update
+-- Update the record of mealdb based on the condition
 
-UPDATE MealDB
-SET category = "Salad" WHERE meal_id = 2307;
+UPDATE MealDB SET category = "Salad" WHERE meal_id = 2307;
+
 SELECT * FROM MealDB;
 
--- Insert
+-- Insert records into mealdb
 
 INSERT into MealDB values(1755, "Beverages", "Thai");
+
 SELECT * FROM MealDB;
 
 -- Drop a table
